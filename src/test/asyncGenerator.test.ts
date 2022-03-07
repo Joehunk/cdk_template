d41efee2fcc50671ed0adfad4b5e366e2b377f86
Charles Crain <charles@paggo.care>
2022-03-06 19:36:10 -0800

    Prototype async generator to saga glue. Tests passing.

--- src/test/asyncGenerator.test.ts
+++ src/test/asyncGenerator.test.ts
@@ -1,4 +1,5 @@
 import { CallEffect, call } from "redux-saga/effects";
+import { unsafeRunAsync } from "../bin/middleware";
 
 async function* asyncGen(): AsyncGenerator<number, void, void> {
   yield 1;
@@ -10,8 +11,29 @@ async function asyncFunc() {
   return 1;
 }
 
+function doNext<T, U>(gen: AsyncGenerator<T, U, void>): Promise<IteratorResult<T, U>> {
+  return gen.next();
+}
+
 function iterateAsync<T, U>(gen: AsyncGenerator<T, U, void>): CallEffect<IteratorResult<T, U>> {
-  return call(gen.next);
+  // The type inferencer wasn't quite amazing enough here.
+  return call(doNext, gen) as CallEffect<IteratorResult<T, U>>;
+}
+
+async function doLog(value: string) {
+  console.log(value);
+}
+
+function* asyncIteratorSaga() {
+  const iterator = iterateAsync(asyncGen());
+  let sum = 0;
+
+  for (let i: IteratorResult<number, void> = yield iterator; !i.done; i = yield iterator) {
+    yield call(doLog, `It is: ${i.value}`);
+    sum += i.value;
+  }
+
+  return sum;
 }
 
 test("Async effect equality", () => {
@@ -36,4 +58,23 @@ test("Async generator iterator equality", () => {
   const iter2 = iterateAsync(gen2);
 
   expect(iter1).toMatchObject(iter2);
+});
+
+test("Async generator saga purely functional test", () => {
+  const saga = asyncIteratorSaga();
+  const iterator = iterateAsync(asyncGen());
+
+  // Notice that the actual implementation of the async generator function is
+  // irrelevant, since the whole thing is considered an effect. This makes me debate
+  // the wisdom of using it since almost by definition an async generator is a thing
+  // that mixes logic and effects, with no way to test the logic separately from the
+  // effects, and if we're doing that then why the heck are we even using redux-saga?
+  expect(saga.next().value).toMatchObject(iterator);
+  expect(saga.next({ done: false, value: 1 }).value).toMatchObject(call(doLog, "It is: 1"));
+});
+
+test("Async generator saga effectful test", async () => {
+  const result = await unsafeRunAsync(asyncIteratorSaga);
+
+  expect(result).toEqual(6);
 });
